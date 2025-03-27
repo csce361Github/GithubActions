b228b06c1130b9c1bb4bde87d879c06cd8e4b173
Maya Wilson <101210194+wilso316@users.noreply.github.com>
2025-03-27 15:10:39 -0500

    test subtract

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -4,7 +4,7 @@ namespace GithubActionsLab;
 public class Addition
 {
 	[TestMethod]
-	public void Add_Valid_Patino()
+	public void Add_Valid_Wilson()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
@@ -12,7 +12,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Invalid_Patino()
+	public void Add_Invalid_Wilson()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
@@ -20,10 +20,36 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Null_Patino()
+	public void Add_Null_Wilson()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
+}
+public class Subtraction
+{
+    [TestMethod]
+    public void Subtract_Valid_Wilson()
+    {
+        Assert.AreEqual(5, Program.Subtract("8", "3"));
+        Assert.AreEqual(5, Program.Subtract("3", "2"));
+        Assert.AreEqual(1, Program.Subtract("3", "2"));
+    }
+
+    [TestMethod]
+    public void Subtract_Invalid_Wilson()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
+    }
+
+    [TestMethod]
+    public void Add_Null_Wilson()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
+    }
 }
