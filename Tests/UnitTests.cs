f9e6eebee0a83631aca2f89f4233e9b6745463b1
Meldermac <142739632+Meldermac@users.noreply.github.com>
2025-03-28 18:49:03 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -4,7 +4,7 @@ namespace GithubActionsLab;
 public class Addition
 {
 	[TestMethod]
-	public void Add_Valid_Patino()
+	public void Add_Valid_Odulate()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
@@ -12,7 +12,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Invalid_Patino()
+	public void Add_Invalid_Odulate()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
@@ -20,7 +20,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Null_Patino()
+	public void Add_Null_Odulate()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -32,7 +32,7 @@ public class Addition
 public class Subtraction
 {
 	[TestMethod]
-	public void Subtract_Valid_Patino()
+	public void Subtract_Valid_Odulate()
 	{
 		Assert.AreEqual(1, Program.Subtract("3", "2"));
 		Assert.AreEqual(5, Program.Subtract("10", "5"));
@@ -40,7 +40,7 @@ public class Subtraction
 	}
 
 	[TestMethod]
-	public void Subtract_Invalid_Patino()
+	public void Subtract_Invalid_Odulate()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
@@ -48,7 +48,7 @@ public class Subtraction
 	}
 
 	[TestMethod]
-	public void Subtract_Null_Patino()
+	public void Subtract_Null_Odulate()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
@@ -60,7 +60,7 @@ public class Subtraction
 public class Multiplication
 {
 	[TestMethod]
-	public void Multiply_Valid_Patino()
+	public void Multiply_Valid_Odulate()
 	{
 		Assert.AreEqual(3, Program.Multiply("3", "1"));
 		Assert.AreEqual(50, Program.Multiply("10", "5"));
@@ -68,7 +68,7 @@ public class Multiplication
 	}
 
 	[TestMethod]
-	public void Multiply_Invalid_Patino()
+	public void Multiply_Invalid_Odulate()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
@@ -76,7 +76,7 @@ public class Multiplication
 	}
 
 	[TestMethod]
-	public void Multiply_Null_Patino()
+	public void Multiply_Null_Odulate()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
@@ -88,7 +88,7 @@ public class Multiplication
 public class Division
 {
 	[TestMethod]
-	public void Divide_Valid_Patino()
+	public void Divide_Valid_Odulate()
 	{
 		Assert.AreEqual(2, Program.Divide("6", "3"));
 		Assert.AreEqual(5, Program.Divide("10", "2"));
@@ -96,7 +96,7 @@ public class Division
 	}
 
 	[TestMethod]
-	public void Divide_Invalid_Patino()
+	public void Divide_Invalid_Odulate()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
@@ -104,7 +104,7 @@ public class Division
 	}
 
 	[TestMethod]
-	public void Divide_Null_Patino()
+	public void Divide_Null_Odulate()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
@@ -116,7 +116,7 @@ public class Division
 public class Power
 {
 	[TestMethod]
-	public void Power_Valid_Patino()
+	public void Power_Valid_Odulate()
 	{
 		Assert.AreEqual(9, Program.Power("3", "2"));
 		Assert.AreEqual(25, Program.Power("5", "2"));
@@ -124,7 +124,7 @@ public class Power
 	}
 
 	[TestMethod]
-	public void Power_Invalid_Patino()
+	public void Power_Invalid_Odulate()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
@@ -132,7 +132,7 @@ public class Power
 	}
 
 	[TestMethod]
-	public void Power_Null_Patino()
+	public void Power_Null_Odulate()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
