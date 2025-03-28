52515d8c301e38f5ad87ceb903ec75fb463fd525
Logan Trost <ltrost2@huskers.unl.edu>
2025-03-28 17:16:51 -0500

    Failed Unit Test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -26,4 +26,32 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
+
+	[TestMethod]
+	public void Subtract_Valid()
+	{
+		Assert.AreEqual(1, Program.Subtract("3", "2"));
+		Assert.AreEqual(0, Program.Subtract("5", "5"));
+		Assert.AreEqual(-2, Program.Subtract("2", "4"));
+	}
+
+	[TestMethod]
+	public void Multiply_Valid()
+	{
+		Assert.AreEqual(5, Program.Multiply("3", "2"));
+	}
+
+	[TestMethod]
+	public void Divide_Valid()
+	{
+		Assert.AreEqual(2, Program.Divide("4", "2"));
+		Assert.AreEqual(1, Program.Divide("3", "3"));
+	}
+
+	[TestMethod]
+	public void Power_Valid()
+	{
+		Assert.AreEqual(8, Program.Power("2", "3"));
+		Assert.AreEqual(1, Program.Power("5", "0"));
+	}
 }
