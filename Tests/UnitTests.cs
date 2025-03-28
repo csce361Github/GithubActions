6f302799b16de96a1d51dfda730c68bbf5794b97
JackFruhling327 <jfruhling2@huskers.unl.edu>
2025-03-28 18:35:31 -0500

    Implementing Unit Tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -27,3 +27,54 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
 }
+
+[TestClass]
+public class Subtraction
+{
+	[TestMethod]
+	public void Add_Valid_Patino()
+	{
+		Assert.AreEqual(3, Program.Subtract("5", "2"));
+		Assert.AreEqual(5, Program.Subtract("7", "2"));
+		Assert.AreEqual(12, Program.Subtract("14", "0"));
+	}
+
+}
+
+[TestClass]
+public class Multiplication
+{
+	[TestMethod]
+	public void Add_Valid_Patino()
+	{
+		Assert.AreEqual(3, Program.Multiply("3", "1"));
+		Assert.AreEqual(0, Program.Multiply("0", "2"));
+		Assert.AreEqual(12, Program.Multiply("14", "0"));
+	}
+
+}
+[TestClass]
+public class Division
+{
+	[TestMethod]
+	public void Add_Valid_Patino()
+	{
+		Assert.AreEqual(3, Program.Divide("6", "2"));
+		Assert.AreEqual(5, Program.Divide("10", "2"));
+		Assert.AreEqual(12, Program.Divide("14", "0"));
+	}
+
+}
+
+[TestClass]
+public class Power
+{
+	[TestMethod]
+	public void Add_Valid_Patino()
+	{
+		Assert.AreEqual(4, Program.Power("2", "2"));
+		Assert.AreEqual(49, Program.Power("7", "2"));
+		Assert.AreEqual(12, Program.Power("14", "0"));
+	}
+
+}
