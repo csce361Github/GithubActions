2a87fdbdbd5426ce3c5c1dd7c193071582863e08
JackFruhling327 <jfruhling2@huskers.unl.edu>
2025-03-28 17:37:40 -0500

    Fixed Power Method (again)

--- Console/Program.cs
+++ Console/Program.cs
@@ -86,6 +86,8 @@ public class Program
 	// Implement this method following a similar pattern as above
 	public static double Power(string x, string y)
 	{    
-        return Math.Pow(x, y);
+       double base = Convert.ToDouble(x);
+	   double exponent = Convert.ToDouble(y);
+	    return Math.Pow(base, exponent);
 	}
 }
