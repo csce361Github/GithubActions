ec58d61704a6a748b40e3272b70f7f2a8d50cead
JackFruhling327 <jfruhling2@huskers.unl.edu>
2025-03-28 19:24:46 -0500

    new power change

--- Console/Program.cs
+++ Console/Program.cs
@@ -86,7 +86,12 @@ public class Program
 	// Implement this method following a similar pattern as above
 	public static double Power(string x, string y)
 	{    
-       double baseValue = Convert.ToDouble(x);
+       if (x == null || y == null)
+    {
+        throw new ArgumentNullException("Neither x or y can be null.");
+    }
+	   
+	   double baseValue = Convert.ToDouble(x);
 	   double exponentValue = Convert.ToDouble(y);
 	   return Math.Pow(baseValue, exponentValue);
 	}
