8bb94fb5242891b627d38281b9af913542a5f3bc
eashbrook21 <elijah.ashbrook@gmail.com>
2025-03-28 19:09:44 -0500

    unit tests update

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -26,4 +26,38 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
+  
+
+    [TestMethod]
+    public void Subtract_Valid_Patino()
+    {
+        Assert.AreEqual(1, Program.Subtract("3", "2"));
+        Assert.AreEqual(-2, Program.Subtract("5", "7"));
+        Assert.AreEqual(0, Program.Subtract("2", "2"));
+    }
+
+    [TestMethod]
+    public void Multiply_Valid_Patino()
+    {
+        Assert.AreEqual(6, Program.Multiply("3", "2"));
+        Assert.AreEqual(35, Program.Multiply("5", "7"));
+        Assert.AreEqual(0, Program.Multiply("2", "0"));
+    }
+
+    [TestMethod]
+    public void Divide_Valid_Patino()
+    {
+        Assert.AreEqual(1.5, Program.Divide("3", "2"), 0.0001);
+        Assert.AreEqual(5, Program.Divide("10", "2"));
+    }
+
+    [TestMethod]
+    public void Power_Valid_Patino()
+    {
+        Assert.AreEqual(9, Program.Power("3", "2"));
+        Assert.AreEqual(1, Program.Power("5", "0"));
+        Assert.AreEqual(0.5, Program.Power("2", "-1"), 0.0001);
+    }
+
+   
 }
